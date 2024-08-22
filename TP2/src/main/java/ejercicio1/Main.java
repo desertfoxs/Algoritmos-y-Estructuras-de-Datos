bb77586ae03f724c1d67d0549f1c08b3126f2de1
desertfoxs <angelolivetti78@gmail.com>
2024-08-22 18:21:35 -0300

    commit integrando el nuevo tp

---
+++ TP2/src/main/java/ejercicio1/Main.java
@@ -0,0 +1,13 @@
+package ejercicio1;
+
+import claseUtilizar.ArbolGeneral;
+import ejercicio2.ListaGenerica;
+import ejercicio2.NodoGenerico;
+import ejercicio2.ListaEnlazadaGenerica;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
