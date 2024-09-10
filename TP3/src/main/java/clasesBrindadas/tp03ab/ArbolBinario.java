3514279751ca3eb13b56c55a79ed056f432f418d
desertfoxs <angelolivetti78@gmail.com>
2024-09-10 00:54:29 -0300

    commit agregando el TP 3 parte 2

---
+++ TP3/src/main/java/clasesBrindadas/tp03ab/ArbolBinario.java
@@ -0,0 +1,119 @@
+package clasesBrindadas.tp03ab;
+
+public class ArbolBinario<T> {
+	private T dato;
+	private ArbolBinario<T> hijoIzquierdo;   
+	private ArbolBinario<T> hijoDerecho; 
+
+	
+	public ArbolBinario() {
+		super();
+	}
+
+	public ArbolBinario(T dato) {
+		this.dato = dato;
+	}
+
+	/*
+	 * getters y setters
+	 * 
+	 */
+	public T getDato() {
+		return dato;
+	}
+
+	public void setDato(T dato) {
+		this.dato = dato;
+	}
+	
+	/**
+	 * Preguntar antes de invocar si tieneHijoIzquierdo()
+	 * @return
+	 */
+	public ArbolBinario<T> getHijoIzquierdo() {
+		return this.hijoIzquierdo;
+	}
+
+	public ArbolBinario<T> getHijoDerecho() {
+		return this.hijoDerecho;
+
+	}
+
+	public void agregarHijoIzquierdo(ArbolBinario<T> hijo) {
+		this.hijoIzquierdo = hijo;
+	}
+
+	public void agregarHijoDerecho(ArbolBinario<T> hijo) {
+		this.hijoDerecho = hijo;
+	}
+
+	public void eliminarHijoIzquierdo() {
+		this.hijoIzquierdo = null;
+	}
+
+	public void eliminarHijoDerecho() {
+		this.hijoDerecho = null;
+	}
+
+	public boolean esVacio() {
+		return this.getDato() == null && !this.tieneHijoIzquierdo() && !this.tieneHijoDerecho();
+	}
+
+	public boolean esHoja() {
+		return (!this.tieneHijoIzquierdo() && !this.tieneHijoDerecho());
+
+	}
+
+	@Override
+	public String toString() {
+		return this.getDato().toString();
+	}
+
+	 
+	public boolean tieneHijoIzquierdo() {
+		return this.hijoIzquierdo!=null;
+	}
+
+	 
+	public boolean tieneHijoDerecho() {
+		return this.hijoDerecho!=null;
+	}
+
+	//modulos nuevos!
+	public int contarHojas() {
+		int numHojas = procesadorContadorHojas(0);
+		return numHojas;
+
+	}
+
+	private int procesadorContadorHojas(int cant) {
+
+		if(esHoja()){
+			return cant + 1;
+		}
+
+		if(tieneHijoIzquierdo()){
+			cant = this.getHijoIzquierdo().procesadorContadorHojas(cant + 1);
+		}
+		if(tieneHijoDerecho()){
+			cant = this.getHijoDerecho().procesadorContadorHojas(cant + 1);
+		}
+
+		return cant;
+	}
+
+
+	public ArbolBinario<T> espejo() {
+		
+		return null;
+
+	}
+
+
+	public void entreNiveles(int n, int m){
+		
+	}
+
+	
+
+}
